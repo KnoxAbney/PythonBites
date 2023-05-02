cef0e3fcdb92a628165a062e40ea8d38fe8dc28c
KnoxAbney <110506819+KnoxAbney@users.noreply.github.com>
2023-05-02 10:36:20 -0400

    ListEditing.py
    a few functions to edit a list.

--- SquareIt.py
+++ SquareIt.py
@@ -1,9 +1,38 @@
 # square the numbers in a list through the power of god and anime
 
-nums = range(11)
-squares = []
-i = 0
+numbers = [4, 8, 15, 16, 23, 42]
 
-for num in nums:
-  squares.append(nums[i]**2)
-  i += 1
+
+def squareIt(nums):
+  squares = []
+  i = 0
+
+  for num in nums:
+    squares.append(nums[i]**2)
+    i += 1
+  return squares
+    
+print(squareIt(numbers))
+
+
+def plusTen(nums):
+  add_ten = []
+  i = 0
+
+  for num in nums:
+    add_ten.append(nums[i]+10)
+    i += 1
+  return add_ten
+    
+print(plusTen(numbers))
+
+def divideTwo(nums):
+  divide_by_two = []
+  i = 0
+
+  for num in nums:
+    divide_by_two.append(nums[i]/2)
+    i += 1
+  return divide_by_two
+    
+print(divideTwo(numbers))
